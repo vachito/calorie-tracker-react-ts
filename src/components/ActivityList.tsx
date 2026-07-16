1d4f73650eb727542171b98f33eb75d908855b38
vachito <vacho19@gmail.com>
2026-07-15 21:13:29 -0600

    identificando la actividad a editar

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,13 +1,15 @@
 import type { Activity } from "../types"
 import { categories } from "../data/categories"
-import { useMemo } from "react"
+import { useMemo, type Dispatch } from "react"
 import {PencilSquareIcon} from '@heroicons/react/24/outline'
+import type { ActivityActions } from "../reducers/activity-reducer"
 
 type ActivityListProps = {
-    activities : Activity[]
+    activities : Activity[],
+    dispatch : Dispatch<ActivityActions>
 }
 
-export default function ActivityList({activities}:ActivityListProps) {
+export default function ActivityList({activities, dispatch}:ActivityListProps) {
     const categoryName = useMemo(()=>
     (category : Activity['category']) => categories.map(cat => cat.id === category ? cat.name : '')
     , [activities])
@@ -41,7 +43,11 @@ export default function ActivityList({activities}:ActivityListProps) {
                 </div>
 
                 <div className="flex gap-5 items-center">
-                    <button type="button" className="cursor-pointer">
+                    <button 
+                        type="button" 
+                        className="cursor-pointer"
+                        onClick={()=> dispatch({type: 'set-activeId', payload: {id: activity.id}})}
+                    >
                         <PencilSquareIcon className="h-8 w-8 text-gray-800"/>
                     </button>
                 </div>
--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -1,16 +1,17 @@
 import type { Activity } from "../types";
 
-export type ActivityActions = {
-    type : 'save-activity',
-    payload : { newActivity : Activity }
-};
+export type ActivityActions = 
+  {type : 'save-activity',payload : { newActivity : Activity }} |
+  {type : 'set-activeId',payload : { id : Activity['id'] }} 
 
 type ActivityState = {
-  activities: Activity[];
+  activities: Activity[]
+  activeId: Activity['id']
 };
 
 export const initialState: ActivityState = {
   activities: [],
+  activeId: ''
 };
 
 export const activityReducer = (
@@ -24,5 +25,12 @@ export const activityReducer = (
        } 
     }
 
+    if (action.type === 'set-activeId'){
+      return{
+        ...state,
+        activeId: action.payload.id
+      }
+    }
+    
     return state
 };
